aa7cec7e76a556c3674c52d7d2650c98f32a1a9e
XXXHake <allenyung@outlook.com>
2019-01-06 05:25:17 +0800

    #2

--- rubick.lua
+++ rubick.lua
@@ -2,6 +2,13 @@ local rubick = {}
 rubick.lastspell = {}
 rubick.spellkey = Menu.AddKeyOption({".SCRIPTS", "Rubick"}, "Rubick Spell Key", Enum.ButtonCode.KEY_SPACE)
 rubick.swapkey = Menu.AddKeyOption({".SCRIPTS", "Rubick"}, "Rubick Swap Key", Enum.ButtonCode.KEY_X)
+rubick.logical = {
+    {name = "crystal_maiden_freezing_field", radius = 835},
+    {name = "earthshaker_echo_slam", radius = 600},
+    {name = "enigma_black_hole", radius = 420},
+    {name = "faceless_void_chronosphere", radius = 425},
+    {name = "tidehunter_ravage", radius = 1250}
+}
 
 function rubick.OnUpdate()
     local self = Heroes.GetLocal()
@@ -21,13 +28,13 @@ function rubick.OnUpdate()
             local name = NPC.GetUnitName(enemy)
             for abilityindex = 0, 24 do
                 local enemyspell = NPC.GetAbilityByIndex(enemy, abilityindex)
-                if enemyspell and Ability.GetCooldown(enemyspell) > 0 then
-                    rubick.lastspell[name] = Ability.GetName(enemyspell)
-                end
-                if spellsteal and Ability.IsReady(spellsteal) and NPC.IsEntityInRange(self, enemy, range) then
-                    if enemyspell and Ability.GetCooldown(enemyspell) > 0 and spell and Ability.GetCooldown(spell) > 0 or NPC.GetAbility(self, "rubick_empty1") or Menu.IsKeyDown(rubick.swapkey) then
-                        if rubick.lastspell[name] ~= nil and spell and Ability.GetName(spell) ~= rubick.lastspell[name] then
-                            Ability.CastTarget(spellsteal, enemy)
+                if enemyspell and Ability.GetCooldown(enemyspell) > 0  then
+                    if spellsteal and Ability.IsReady(spellsteal) and NPC.IsEntityInRange(self, enemy, range) then
+                        if Ability.GetCooldown(enemyspell) > 0 and spell and Ability.GetCooldown(spell) > 0 or NPC.GetAbility(self, "rubick_empty1") or Menu.IsKeyDown(rubick.swapkey) then
+                            if spell and Ability.GetName(spell) ~= Ability.GetName(enemyspell) and (not rubick.lastspell[Ability.GetName(enemyspell)] or GameRules.GetGameTime() - rubick.lastspell[Ability.GetName(enemyspell)] > Ability.GetCooldownLength(enemyspell)) then
+                                Ability.CastTarget(spellsteal, enemy)
+                                rubick.lastspell[Ability.GetName(enemyspell)] = GameRules.GetGameTime()
+                            end
                         end
                     end
                 end
@@ -37,49 +44,67 @@ function rubick.OnUpdate()
     local enemy = Input.GetNearestHeroToCursor(Entity.GetTeamNum(self), Enum.TeamType.TEAM_ENEMY)
     if enemy and Menu.IsKeyDown(rubick.spellkey) and not NPC.IsChannellingAbility(self) then
         if spell and Ability.IsReady(spell) then
-            if (Ability.GetBehavior(spell) & Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_NO_TARGET) ~= 0 then
-                if Ability.GetName(spell) == "earthshaker_enchant_totem" and NPC.GetItem(self, "item_ultimate_scepter") then
-                    Ability.CastPosition(spell, Entity.GetAbsOrigin(enemy))
-                else
-                    Ability.CastNoTarget(spell)
-                end
-            end
-            if (Ability.GetBehavior(spell) & Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_UNIT_TARGET) ~= 0 then
-                if (Ability.GetTargetTeam(spell) & Enum.TargetTeam.DOTA_UNIT_TARGET_TEAM_ENEMY) ~= 0 then
-                    Ability.CastTarget(spell, enemy)
-                else
-                    Ability.CastTarget(spell, self)
-                end
-            end
-            if (Ability.GetBehavior(spell) & Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_POINT) ~= 0 then
-                if NPC.IsRunning(enemy) then
-                    local speed = NPC.GetMoveSpeed(enemy)
-                    local angle = Entity.GetRotation(enemy)
-                    local offset = Angle(0, 45, 0)
-                    angle:SetYaw(angle:GetYaw() + offset:GetYaw())
-                    local x,y,z = angle:GetVectors()
-                    local direction = x + y + z
-                    
-                    direction:SetZ(0)
-                    direction:Normalize()
-                    if Ability.GetName(spell) == "invoker_sun_strike" then
-                        direction:Scale(speed * 2)
+            for _, logicspell in pairs(rubick.logical) do
+                if logicspell and spell and Ability.GetName(spell) == logicspell.name then
+                    islogicspell = true
+                    spellradius = logicspell.radius
+                    if (Ability.GetBehavior(spell) & Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_CHANNELLED) ~= 0 then
+                        ischannelling = true
                     else
-                        direction:Scale(speed)
+                        ischannelling = false
                     end
-                    local origin = NPC.GetAbsOrigin(enemy)
-                    local pos = origin + direction
-                    Ability.CastPosition(spell, pos)
-                else
-                    Ability.CastPosition(spell, Entity.GetAbsOrigin(enemy))
+                end
+            end
+            if not islogicspell or islogicspell and NPC.IsEntityInRange(self, enemy, spellradius - 60) then
+                if (Ability.GetBehavior(spell) & Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_NO_TARGET) ~= 0 then
+                    if Ability.GetName(spell) == "earthshaker_enchant_totem" and NPC.GetItem(self, "item_ultimate_scepter") then
+                        Ability.CastPosition(spell, Entity.GetAbsOrigin(enemy))
+                    else
+                        Ability.CastNoTarget(spell)
+                    end
+                end
+                if (Ability.GetBehavior(spell) & Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_UNIT_TARGET) ~= 0 then
+                    if (Ability.GetBehavior(spell) & Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_AOE) ~= 0 then
+                        Ability.CastTarget(spell, enemy)
+                    end
+                    if (Ability.GetTargetTeam(spell) & Enum.TargetTeam.DOTA_UNIT_TARGET_TEAM_ENEMY) ~= 0 then
+                        Ability.CastTarget(spell, enemy)
+                    else
+                        Ability.CastTarget(spell, self)
+                    end
+                end
+                if (Ability.GetBehavior(spell) & Enum.AbilityBehavior.DOTA_ABILITY_BEHAVIOR_POINT) ~= 0 then
+                    if NPC.IsRunning(enemy) then
+                        local speed = NPC.GetMoveSpeed(enemy)
+                        local angle = Entity.GetRotation(enemy)
+                        local offset = Angle(0, 45, 0)
+                        angle:SetYaw(angle:GetYaw() + offset:GetYaw())
+                        local x,y,z = angle:GetVectors()
+                        local direction = x + y + z
+                        
+                        direction:SetZ(0)
+                        direction:Normalize()
+                        if Ability.GetName(spell) == "invoker_sun_strike" then
+                            direction:Scale(speed * 2)
+                        else
+                            direction:Scale(speed)
+                        end
+                        local origin = NPC.GetAbsOrigin(enemy)
+                        local pos = origin + direction
+                        Ability.CastPosition(spell, pos)
+                    else
+                        Ability.CastPosition(spell, Entity.GetAbsOrigin(enemy))
+                    end
                 end
             end
         end
-        if telekinesis and Ability.IsReady(telekinesis) then
-            Ability.CastTarget(telekinesis, enemy)
-        end
-        if fadebolt and Ability.IsReady(fadebolt) then
-            Ability.CastTarget(fadebolt, enemy)
+        if not spell or spell and not ischannelling then
+            if telekinesis and Ability.IsReady(telekinesis) then
+                Ability.CastTarget(telekinesis, enemy)
+            end
+            if fadebolt and Ability.IsReady(fadebolt) then
+                Ability.CastTarget(fadebolt, enemy)
+            end
         end
     end
 end
